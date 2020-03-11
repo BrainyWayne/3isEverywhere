74687679375ea14be97b5f98ede8c5e610ef0040
Francis Eshun <franciseshun@Franciss-MacBook-Pro.local>
2020-03-11 17:01:43 +0000

    Inital code

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int prevn;
+int holder[1000];
+int occurences[10] = {0,0,0,0,0,0,0,0,0};
+
+int main() {
+   cout << "Enter the last number" << endl;
+   cin >> n;
+   prevn = n;
+   for (int i = 0; i <= n; i++){
+
+       prevn--;
+
+
+       //breaking down number into individual numbers
+       int b = 0;
+       while (n > 0){
+        holder[b] = n % 10;
+
+        //Checking the value broken down
+        switch(holder[b]){
+            case 0:
+                occurences[0]++;
+                break;
+            case 1:
+                occurences[1]++;
+                break;
+            case 2:
+                occurences[2]++;
+                break;
+            case 3:
+                occurences[3]++;
+                break;
+            case 4:
+                occurences[4]++;
+                break;
+            case 5:
+                occurences[5]++;
+                break;
+            case 6:
+                occurences[6]++;
+                break;
+            case 7:
+                occurences[7]++;
+                break;
+            case 8:
+                occurences[8]++;
+                break;
+            case 9:
+                occurences[9]++;
+                break;
+
+            default: cout << "An error has occured";
+
+        }
+
+        n/=10;
+        b++;
+           n = prevn;
+    }
+
+
+        for(int c = 0; c <= 9; c++){
+            cout << c << " appeared " << occurences[c] << " times." << endl;
+
+        }
+
+
+
+   }
+
+//    int a = 12348;
+//    int b = 0;
+//    while (a > 0){
+//        holder[b] = a % 10;
+//        a/=10;
+//        b++;
+//    }
+//
+//    for(int i = 0; i< b; i++){
+//        cout << holder[i] << endl;
+//    }
+    return 0;
+}
