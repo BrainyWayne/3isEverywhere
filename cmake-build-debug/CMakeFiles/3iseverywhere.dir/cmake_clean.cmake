ac177058eda1313854e13df03658111a56c8c075
Francis Eshun <franciseshun@Franciss-MacBook-Pro.local>
2020-03-11 17:17:51 +0000

    Initial working code

---
+++ cmake-build-debug/CMakeFiles/3iseverywhere.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "3iseverywhere"
+  "3iseverywhere.pdb"
+  "CMakeFiles/3iseverywhere.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3iseverywhere.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
